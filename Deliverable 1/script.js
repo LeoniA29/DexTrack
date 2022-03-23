faeb996f90d0eb98cd581cbd09eb7105eccf0521
Leoni Angela <leoni.angela@student.unimelb.edu.au>
2022-03-23 17:14:10 +1100

    added image and video resources into New Design.html

--- Deliverable 1/script.js
+++ Deliverable 1/script.js
@@ -13,29 +13,17 @@
     }
           
 // When the user clicks on the button, scroll to the top of the document
-    function topFunction() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
+    function scrollToTop() {
+
+      window.scrollTo({top: 0, behavior: 'smooth'});
+      
     }
-/*
-    <script>
-//Get the button
-var mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+// Function for Read More button
+    function scrollReadMore() {
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+      window.scrollTo({top: 450, behavior: 'smooth'});
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-</script> */
+    }
+
+    
