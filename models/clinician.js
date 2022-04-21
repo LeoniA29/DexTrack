660ae74e6aa22f537e174ccb90b32c3dca11474b
leonia <leonia@student.unimelb.edu.au>
2022-04-21 17:16:38 +1000

    update clinician.js

--- models/clinician.js
+++ models/clinician.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const schema = new mongoose.Schema({
  first_name: String,
  last_name: String,
+ patient_list: Array
 })
 
 const Clinician = mongoose.model('Clinician', schema)
