79ce4cbbf6d17bb77ca4a00f6307af103c11295b
leonia <leonia@student.unimelb.edu.au>
2022-04-21 17:11:49 +1000

    see if people gets this

--- routes/patientRouter.js
+++ routes/patientRouter.js
@@ -6,16 +6,12 @@ const patientRouter = express.Router()
 // import people controller functions
 const patientController = require('../controllers/patientController')
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 // sample patient routes
 patientRouter.get('/', patientController.getAllPatients)
 patientRouter.get('/:patient_id', patientController.getPatientById)
 
 module.exports = patientRouter
-=======
-=======
->>>>>>> ecfd7a40401f8f5a7bc3089d6a57c439b6ea339c
+
 // add a route to handle the GET request for all people data
 patientRouter.get('/', patientController.getAllPatientData)
 
@@ -30,7 +26,4 @@ patientRouter.post('/register', patientController.insertData)
 
 // export the router
 module.exports = patientRouter
-<<<<<<< HEAD
->>>>>>> ecfd7a40401f8f5a7bc3089d6a57c439b6ea339c
-=======
->>>>>>> ecfd7a40401f8f5a7bc3089d6a57c439b6ea339c
+
