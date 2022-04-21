9f5c3a6fca63c3b43443e87ed535b15ed73a7cce
leonia <leonia@student.unimelb.edu.au>
2022-04-21 17:08:20 +1000

    update patient model

--- models/Patient.js
+++ models/Patient.js
@@ -1,17 +1,54 @@
 const mongoose = require('mongoose')
-
 const schema = new mongoose.Schema({
-    givenName: { type: String, required: true },
-    familyName: { type: String, required: true },
-    address: { type: String, required: true },
-    dob: { type: Date, required: true },
-    email: { type: String, required: true },
-    screenName: { type: String, required: true },
-    shortBio: String,
-    patientData: Array
+ first_name: String,
+ last_name: String,
+ email: String,
+ sex: {
+    type: String,
+    enum: ['Female','Male','Prefer not to answer'],
+    default: 'Prefer not to answer'
+ },
+ dob: Date,
+ phone: String,
+ occupation: String,
+ address: String,
+ postcode: String,
+ // this is unique from the clinician to each patient
+ clinician_message: String,
+  // array of objects for the patient defined in the schema below
+ clincian_notes: [note],
+ threshold_list: [threshold],
+ input_data: [data],
 })
+
+// schema for notes
+const note = new mongoose.Schema({
+   note_content: String,
+   note_date: Date
+});
+
+// schema for threshold
+const threshold = new mongoose.Schema({
+   th_low: Double,
+   th_high: Double,
+   th_required: Boolean,
+   th_type: {
+      type: String,
+      enum: ['glucose', 'insulin', 'weight', 'steps']
+   }
+});
+
+// schema for data input
+const data = new mongoose.Schema({
+   data_entry: Double,
+   data_comment: String,
+   data_date: Date,
+   data_type: {
+      type: String,
+      enum: ['glucose', 'insulin', 'weight', 'steps']
+   }
+});
+
 
 const Patient = mongoose.model('Patient', schema)
 module.exports = Patient
-
-
