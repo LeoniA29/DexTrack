9979585a53e88260624d89b6c10d0e34cedc3fb1
leonia <leonia@student.unimelb.edu.au>
2022-04-29 21:06:14 +1000

    fixed threshold helper

--- views/helpers/exceedThreshold.js
+++ views/helpers/exceedThreshold.js
@@ -5,34 +5,38 @@ module.exports = function(handlebars) {
 
         for (var i in threshold_list) {
 
-            // checks if threshold data exists
-            if (threshold_list[i].high) {
-
             // finds the threshold for that data input
-                if (threshold_list[i].type == 'glucose') {
+            if ((threshold_list[i].type == 'glucose') && (data.data_type == 'glucose')) {
 
-                    // checks if data exceeds threshold or not
-                    if (data > threshold_list[i].high) {
-                        th_boolean = true;
-                    }   
+                // checks if data exceeds threshold or not and if threshold exists
+                if ((data.data_entry > threshold_list[i].high) && (threshold_list[i].high) ) {
+                    th_boolean = true;
+                } else if ((data.data_entry < threshold_list[i].low) && (threshold_list[i].low)) {
+                    th_boolean = true;
                 }
+            }
 
-                if (threshold_list[i].type == 'weight') {
-                    if (data > threshold_list[i].high) {
-                        th_boolean = true;
-                    }
+            if ((threshold_list[i].type == 'weight') && (data.data_type == 'weight')) {
+                if ((data.data_entry > threshold_list[i].high) && (threshold_list[i].high) ) {
+                    th_boolean = true;
+                } else if ((data.data_entry < threshold_list[i].low) && (threshold_list[i].low)) {
+                    th_boolean = true;
                 }
+            }
 
-                if (threshold_list[i].type == 'steps') {
-                    if (data > threshold_list[i].high) {
-                        th_boolean = true;
-                    }   
+            if ((threshold_list[i].type == 'steps') && (data.data_type == 'steps')) {
+                if ((data.data_entry > threshold_list[i].high) && (threshold_list[i].high) ) {
+                    th_boolean = true;
+                } else if ((data.data_entry < threshold_list[i].low) && (threshold_list[i].low)) {
+                    th_boolean = true;
                 }
+            }
 
-                if (threshold_list[i].type == 'insulin') {
-                    if (data > threshold_list[i].high) {
-                        th_boolean = true;
-                    }
+            if ((threshold_list[i].type == 'insulin') && (data.data_type == "insulin")) {
+                if ((data.data_entry > threshold_list[i].high) && (threshold_list[i].high) ) {
+                    th_boolean = true;
+                } else if ((data.data_entry < threshold_list[i].low) && (threshold_list[i].low)) {
+                    th_boolean = true;
                 }
             }
         }
